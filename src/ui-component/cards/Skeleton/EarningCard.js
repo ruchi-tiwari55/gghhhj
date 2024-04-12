d8d3090df67b5cde665c386b26ecd51f0fea7bff
Aman <amankumar94719@gmail.com>
2024-04-12 17:31:09 +0530

    first commit

---
+++ src/ui-component/cards/Skeleton/EarningCard.js
@@ -0,0 +1,16 @@
+// material-ui
+import { Card, CardContent, Grid } from '@mui/material';
+
+// ==============================|| SKELETON - EARNING CARD ||============================== //
+
+const EarningCard = () => (
+  <Card>
+    <CardContent>
+      <Grid container direction="column">
+        
+      </Grid>
+    </CardContent>
+  </Card>
+);
+
+export default EarningCard;
